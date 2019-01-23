e254e50888a14f0a7898413f020e5d9c4169a77c
Mr.Deer <Yiwei.Lu@deer-young.top>
2019-01-23 22:44:22 +0800

    devel: Update ssh-pki.sh read conf file and output a auto install pkg.
    Signed-off-by: Mr.Deer <Yiwei.Lu@deer-young.top>

--- src/ssh-pki.sh
+++ src/ssh-pki.sh
@@ -8,22 +8,235 @@
 
 #set -e 
 
+ssh_pki_print()
+{
+    echo -e "\033[1;3$1m$2\033[0m"
+}
+
+ssh_pki_conf_set()
+{
+    key=$2
+    value=$3
+
+    chmod 644 $CONF_FILE
+    case $1 in
+        0|3)
+            # nomal and no value
+            sed -i "/^${key}/c${key}=${value}" $CONF_FILE
+            ;;
+        1)
+            # no file
+            touch $CONF_FILE
+            echo "${key}=${value}" >> $CONF_FILE
+            ;;
+        2)
+            # no key
+            echo "${key}=${value}" >> $CONF_FILE
+            ;;
+        *)
+            chmod 444 $CONF_FILE
+            return 1
+            ;;
+    esac
+    chmod 444 $CONF_FILE
+    return 0
+}
+
+ssh_pki_conf_get()
+{
+    key=$1
+
+    if [ ! -f $CONF_FILE ]; then
+        # no file key value
+        echo "No config file [$CONF_FILE]"
+        return 1
+    fi
+
+    value=`grep $key $CONF_FILE`
+    if [ $? -ne 0 ]; then
+        # no key
+        echo "No config key [$key]"
+        return 2
+    fi
+    if [ -z "$value" ]; then
+        echo "No config value for key [$key]"
+        return 3
+    fi
+
+    value=${value#*=}
+    echo $value
+}
+
+ssh_pki_package()
+{
+
+    tempdir=${FILE_NAME}_PKG
+    pkg_install=pkg_install.sh
+    tgz=${FILE_NAME}.tgz
+
+    if [ -d $tempdir ]; then
+        rm -rf $tempdir
+    fi
+    mkdir $tempdir
+
+cat > $pkg_install << 'EOF'
+#!/bin/bash
+
+tmpdir=/tmp/ssh-pki-upkg
+prog="${tmpdir}/install.sh"
+if [ -d $tmpdir ]; then
+    rm -rf $tmpdir
+fi
+mkdir -p $tmpdir
+if tail -n +21 "$0"|tar -zxpf - -C ${tmpdir}; then
+    cd $tmpdir
+    chmod 755 $prog
+    if [ x$1 = 'xunpkg' ]; then
+        exit 0
+    fi
+    source $prog $*
+    exit 0
+else
+    echo -e "\033[1;31mCan't decompress $0\033[0m"
+    exit 1
+fi
+EOF
+
+    ssh_pki_print 3 "Packaging..."
+
+    if [ $USERF -eq 1 ]; then
+        peer_ca_file=`ssh_pki_conf_get host_ca_file`
+    else
+        peer_ca_file=`ssh_pki_conf_get user_ca_file`
+    fi
+    if [ $? -ne 0 ]; then
+        ssh_pki_print 1 "$peer_ca_file"
+    fi
+    if [ ! -f $peer_ca_file ]; then
+        ssh_pki_print 1 "Get peer CA file error!"
+    fi
+
+cat > ${tempdir}/install.sh << 'EOF'
+#!/bin/bash
+
+# Script to install certs.
+
+set -e
+
+EOF
+    if [ $USERF -eq 1 ]; then
+        echo "Packaging user files."
+        cp ${peer_ca_file}.pub $tempdir/
+        mv ${FILE_NAME} $tempdir/
+        mv ${FILE_NAME}.pub $tempdir/
+        mv ${FILE_NAME}-cert.pub $tempdir/
+        echo "mkdir ~/.ssh/" >> ${tempdir}/install.sh
+        echo "cp ${peer_ca_file}.pub ~/.ssh/" >> ${tempdir}/install.sh
+        echo "cp ${FILE_NAME} ~/.ssh/" >> ${tempdir}/install.sh
+        echo "cp ${FILE_NAME}.pub ~/.ssh/" >> ${tempdir}/install.sh
+        echo "cp ${FILE_NAME}-cert.pub ~/.ssh/" >> ${tempdir}/install.sh
+cat >> ${tempdir}/install.sh << 'EOF'
+cd ~/.ssh/
+if [ -f id_rsa ]; then
+    mv id_rsa id_rsa.bak
+fi
+if [ -f id_rsa.pub ]; then
+    mv id_rsa.pub id_rsa.pub.bak
+fi
+if [ -f id_rsa-cert.pub ]; then
+    mv id_rsa-cert.pub id_rsa-cert.pub.bak
+fi
+EOF
+        echo "ln -s ${FILE_NAME} id_rsa" >> ${tempdir}/install.sh
+        echo "ln -s ${FILE_NAME}.pub id_rsa.pub" >> ${tempdir}/install.sh
+        echo "ln -s ${FILE_NAME}-cert.pub id_rsa-cert.pub" >> ${tempdir}/install.sh
+        echo "cp known_hosts known_hosts.bak" >> ${tempdir}/install.sh
+        echo "echo -n \"@cert-authority * \" >> known_hosts" >> ${tempdir}/install.sh
+        echo "cat ${peer_ca_file}.pub >> known_hosts" >> ${tempdir}/install.sh
+    else
+        echo "Packaging host files."
+        cp ${peer_ca_file}.pub $tempdir/
+        mv ${FILE_NAME} $tempdir/
+        mv ${FILE_NAME}.pub $tempdir/
+        mv ${FILE_NAME}-cert.pub $tempdir/
+cat >> ${tempdir}/install.sh << 'EOF'
+if [ `whoami` != "root" ]; then
+    echo -e "\033[1;31mPlease run by root!\033[0m"
+    localdir=`pwd`
+    cd ..
+    rm -rf $localdir
+    exit 1
+fi
+EOF
+        echo "" >> ${tempdir}/install.sh
+        echo "cp ${peer_ca_file}.pub /etc/ssh/" >> ${tempdir}/install.sh
+        echo "cp ${FILE_NAME} /etc/ssh/" >> ${tempdir}/install.sh
+        echo "cp ${FILE_NAME}.pub /etc/ssh/" >> ${tempdir}/install.sh
+        echo "cp ${FILE_NAME}-cert.pub /etc/ssh/" >> ${tempdir}/install.sh
+        echo "cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak" >> ${tempdir}/install.sh
+        echo "echo \"TrustedUserCAKeys /etc/ssh/${peer_ca_file}.pub\" >> /etc/ssh/sshd_config" >> ${tempdir}/install.sh
+        echo "echo \"HostCertificate /etc/ssh/${FILE_NAME}-cert.pub\" >> /etc/ssh/sshd_config" >> ${tempdir}/install.sh
+    fi
+    echo "echo -e \"\\033[1;32mInstallation is complete!\\033[0m\"" >> ${tempdir}/install.sh
+    chmod 755 ${tempdir}/install.sh
+    cd $tempdir
+    tar zcvf $tgz *
+    mv ${tgz} ..
+    cd ..
+    cat $pkg_install $tgz > ${FILE_NAME}.install
+    chmod 755 ${FILE_NAME}.install
+    mv ${FILE_NAME}.install $tempdir
+    rm $tgz $pkg_install
+    ssh_pki_print 2 "Packaged successfully !!!\n"
+}
+
 ssh_pki_gen_key()
 {
-    echo -e "\033[1;33mGen new key [${KEY_NAME}].\033[0m\n"
+    ssh_pki_print 3 "Gen new key [${KEY_NAME}]..."
     FILE_NAME=${DATE}_${KEY_NAME}
 
-    ssh-keygen -t rsa -C ${KEY_NAME} -b 4096 -f ${KEY_NAME}
+    ssh-keygen -t rsa -C ${KEY_NAME} -b $KEY_BIT -f ${KEY_NAME}
+    echo
 
+    chmod 400 ${KEY_NAME}
+    chmod 444 ${KEY_NAME}.pub
+
+    if [ $GENCAF -eq 1 ]; then
+        return
+    fi
     mv ${KEY_NAME} ${FILE_NAME}
     mv ${KEY_NAME}.pub ${FILE_NAME}.pub
+}
 
-    echo
+ssh_pki_gen_ca()
+{
+    case $KEY_NAME in
+        *"user"*)
+            ssh_pki_conf_get user_ca_file
+            ret=$?
+            ssh_pki_conf_set $ret user_ca_file $KEY_NAME
+            ;;
+        *"host"*)
+            ssh_pki_conf_get host_ca_file
+            ret=$?
+            ssh_pki_conf_set $ret host_ca_file $KEY_NAME
+            ;;
+        *)
+            ssh_pki_print 1 "Please input string contains \"user\" or \"host\"!"
+            exit 1
+            ;;
+    esac
+    ssh_pki_gen_key
 }
 
 ssh_pki_sign_key()
 {
-    echo -e "\033[1;33mSign key [${KEY_NAME}] by [${CA_FILE}] for [${WEEKS}] weeks.\033[0m\n"
+    year=`date  +%Y%m`
+    FILE_NAME=${FILE_NAME%.pub*}
+
+    if [ $GENF -eq 0 ]; then
+        KEY_NAME=${KEY_NAME#*_}
+    fi
 
     if [ ${WEEKS} -gt 1 ]; then
         validity="-V +${WEEKS}w"
@@ -31,17 +244,46 @@ ssh_pki_sign_key()
         validity=""
     fi
 
-    if [ $HOSTF -eq 0 ]; then
+    if [ ! -f $CA_FILE ]; then
+        if [ $USERF -eq 1 ]; then
+            CA_FILE=`ssh_pki_conf_get user_ca_file`
+        else
+            CA_FILE=`ssh_pki_conf_get host_ca_file`
+        fi
+    fi
+    ret=$?
+    if [ $ret -eq 0 ]; then
+        ssh_pki_print 3 "CA file not exist. Do you want sign by [$CA_FILE]?"
+        read YN
+        case $YN in
+            n|no)
+                return
+                ;;
+        esac
+    else
+        ssh_pki_print 1 "[$ret]$CA_FILE"
+        exit 1
+    fi
+
+    if [ ! -f $CA_FILE ]; then
+        ssh_pki_print 1 "Get CA file error!"
+        exit 1
+    fi
+
+    ssh_pki_print 3 "Sign key [${KEY_NAME}] by [${CA_FILE}] for [${WEEKS}] weeks...\n"
+
+    if [ $USERF -eq 1 ]; then
         ssh-keygen -s ${CA_FILE} -I ${KEY_NAME}--${CA_FILE} -n ${KEY_NAME} ${validity} ${FILE_NAME}.pub
     else
         ssh-keygen -s ${CA_FILE} -I ${KEY_NAME}--${CA_FILE} -h -n ${KEY_NAME} ${validity} ${FILE_NAME}.pub
     fi
 
     if [ $? -eq 0 ]; then
-        echo -e "\033[1;32mSign key success!\033[0m"
+        ssh_pki_print 2 "Sign key success!\n"
         ssh-keygen -Lf ${FILE_NAME}-cert.pub
+        ssh_pki_package
     else
-        echo -e "\033[1;31mSign key failed!\033[0m"
+        ssh_pki_print 1 "Sign key failed!\n"
     fi
 }
 
@@ -58,36 +300,50 @@ ssh_pki_help()
 {
     echo -e "\nDeer SSH PKI usage:"
     echo
-    echo -e "\t-u\tuser\tinput username"
-    echo -e "\t-h\thost\tinput hostname"
+    echo -e "\t-c\tgen ca\t\tinput ca name"
+    echo -e "\t-u\tuser\t\tinput username(login name or authentication string)"
+    echo -e "\t-h\thost\t\tinput hostname(url or ip)"
     echo
-    echo -e "\t-g\tgen\tgenerate a new key"
-    echo -e "\t-i\tinkey\tinput key file name"
-    echo -e "\t-s\tsign\tinput ca file name"
+    echo -e "\t-s\tsign\t\tinput ca file name"
     echo
-    echo -e "\t-y\tyear\tinput years default is 1"
+    echo -e "\t-y\tyear\t\tinput years default is 1"
+    echo -e "\t-b\tkey bits\tinput key bits default is 2048"
 }
 
 
-CA_FILE=""
-FILE_NAME=""
-YEARF=0
-YEARS=0
+# Main
 GENF=0
-INPF=0
-SIGNF=0
+GENCAF=0
 USERF=0
 HOSTF=0
+SIGNF=0
+YEARF=0
 
-DATE=`/bin/date +%Y%m%d-%H%M%S`
+YEARS=0
 
-# main
+KEY_BIT=2048
+
+CA_FILE=""
+FILE_NAME=""
+USER_CA_FILE=""
+HOST_CA_FILE=""
+
+PROG_DIR=$0
+PROG_DIR=${PROG_DIR%/*}
+CONF_FILE=${PROG_DIR}/ssh_pki.conf
+
+DATE=`date +%Y%m%d-%H%M%S`
+
 if [ $# -lt 1 ]; then
     ssh_pki_help
     exit 1
 fi
-while getopts "u:h:gi:s:y:" opt; do
+while getopts "c:u:h:s:y:b:" opt; do
     case $opt in
+        c)
+            GENCAF=1
+            KEY_NAME=$OPTARG
+            ;;
         u)
             USERF=1
             KEY_NAME=$OPTARG
@@ -95,13 +351,6 @@ while getopts "u:h:gi:s:y:" opt; do
         h)
             HOSTF=1
             KEY_NAME=$OPTARG
-            ;;
-        g)
-            GENF=1
-            ;;
-        i)
-            INPF=1
-            FILE_NAME=$OPTARG
             ;;
         s)
             SIGNF=1
@@ -111,6 +360,14 @@ while getopts "u:h:gi:s:y:" opt; do
             YEARF=1
             YEARS=$OPTARG
             ;;
+        b)
+            ssh_pki_check_num $OPTARG
+            if [ $? -ne 0 ]; then
+                ssh_pki_print 1 "Input key bits error [$OPTARG]"
+            else
+                KEY_BIT=$OPTARG
+            fi
+            ;;
         \?)
             ssh_pki_help
             exit 1
@@ -118,12 +375,13 @@ while getopts "u:h:gi:s:y:" opt; do
     esac
 done
 
-if [ `expr $USERF + $HOSTF` -ne 1 ]; then
-    echo "Error: Not input user or host name."
-    exit 1
+if [ $GENCAF -eq 1 ]; then
+    ssh_pki_gen_ca
+    exit 0
 fi
-if [ `expr $GENF + $INPF` -ne 1 ]; then
-    echo "Error: Not gen or input key."
+
+if [ `expr $USERF + $HOSTF` -ne 1 ]; then
+    ssh_pki_print 1  "Error! Please input user or host name."
     exit 1
 fi
 
@@ -132,15 +390,19 @@ if [ $YEARF -eq 0 ]; then
 else
     ssh_pki_check_num $YEARS
     if [ $? -ne 0 ]; then
-        echo "Year input error!"
+        ssh_pki_print 1 "Year input error!"
         exit 1
     fi
     WEEKS=$[YEARS*52]
 fi
 
-if [ $GENF -eq 1 ]; then
+if [ -f $KEY_NAME ]; then
+    FILE_NAME=$KEY_NAME
+else
+    GENF=1
     ssh_pki_gen_key
 fi
+
 if [ $SIGNF -eq 1 ]; then
     ssh_pki_sign_key
 fi
